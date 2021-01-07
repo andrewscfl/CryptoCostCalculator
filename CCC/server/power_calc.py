b64a2675934c4568c3041e0074f4f6bd290e5e97
Andrew <andrewscaldwell@gmail.com>
2021-01-06 21:24:40 -0500

    removed some things

--- CCC/server/power_calc.py
+++ CCC/server/power_calc.py
@@ -1,6 +1,6 @@
 import requests
 
-key =  'ZAc3ebvjzWcOmWR8YZJrBnjt4EfZ0gEFs15JncIK'
+key =  "" #add key here in production
 
 def running_avg(new_val, my_lat=35.45, my_long=-82.98):
     avg_power_rates = requests.get('https://developer.nrel.gov/api/utility_rates/v3.json?api_key=' + key + '&lat=' + str(my_lat) +'&lon=' + str(my_long) +'')
