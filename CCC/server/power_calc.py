2631ffab572e72f50b98c6893fb290565a721b1d
Andrew <andrewscaldwell@gmail.com>
2021-01-06 21:34:58 -0500

    patched something

--- CCC/server/power_calc.py
+++ CCC/server/power_calc.py
@@ -1,6 +1,7 @@
 import requests
+import get_cred
 
-key =  "" #add key here in production
+key =  get_cred.return_power_api_key() 
 
 def running_avg(new_val, my_lat=35.45, my_long=-82.98):
     avg_power_rates = requests.get('https://developer.nrel.gov/api/utility_rates/v3.json?api_key=' + key + '&lat=' + str(my_lat) +'&lon=' + str(my_long) +'')
